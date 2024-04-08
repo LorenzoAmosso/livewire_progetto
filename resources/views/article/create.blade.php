2aab017e71b85f84e81918256b40dc8453130f95
lorenzoamosso <lorenzoamossoo@gmail.com>
2024-04-08 22:19:31 +0200

    navbar

---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+                <h1 class="text-center">
+                     Crea nuovo articolo
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+                <h1 class="text-center">
+                    
+                </h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6">
+            <h1 class="text-center">
+                Primo progetto Livewire 
+            </h1>
+        </div>
+    </div>
+</div>
+</x-layout>
